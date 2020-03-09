767c30cb3d7b06737a118084854125dc31936fad
Patryk <pat.grzybala@gmail.com>
2020-03-09 23:21:39 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",  # Replace with your own username
+    name="numerical-algorithms-pgrzy",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Patryk Grzybala",
+    author_email="pat.grzybala@gmail.com",
+    description="Project with problems from Numerical Algorithms classes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Nevrast/numercial_algorithms",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
