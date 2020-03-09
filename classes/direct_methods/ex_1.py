05df7d67da8d5c5a97cf563073ff50f7cf5ae36e
Patryk <pat.grzybala@gmail.com>
2020-03-09 23:16:44 +0100

    fixed algorithm for backward substitution

--- classes/direct_methods/ex_1.py
+++ classes/direct_methods/ex_1.py
@@ -1,6 +1,5 @@
 import numpy as np
-
-from classes.utils.utils import backward_substitution
+import copy
 
 
 def gaussian_elimination(A: np.ndarray, b: np.ndarray) -> np.ndarray:
@@ -8,6 +7,25 @@ def gaussian_elimination(A: np.ndarray, b: np.ndarray) -> np.ndarray:
     for i in range(B.shape[0]):
         B[i] = B[i] - (B[i][i - 1] / B[i - 1][i - 1]) * B[i - 1]
     return B[:, :-1], B[:, -1:]
+
+
+def backward_substitution(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    n = b.shape[0]
+    x = np.zeros_like(b)
+
+    for i in range(n - 1, -1, -1):
+        x[i] = (b[i] - sum(calculate_row(row=A[i], x=x)[i + 1:] if i != n - 1 else [])) / A[i, i]
+        print(A[i, i])
+
+    return x.flat
+
+
+def calculate_row(row: np.ndarray, x: np.ndarray) -> np.ndarray:
+    row = copy.deepcopy(row)
+    for index, x_value in enumerate(x):
+        if x_value != 0:
+            row[index] *= x_value
+    return row
 
 
 if __name__ == "__main__":
@@ -19,5 +37,4 @@ if __name__ == "__main__":
     x, B = gaussian_elimination(A=A, b=b)
     my_result = backward_substitution(A=x, b=B)
     builtin_result = np.linalg.solve(a=A, b=b)
-    print(my_result == builtin_result)
-    pass
+    assert all(my_result == builtin_result)
