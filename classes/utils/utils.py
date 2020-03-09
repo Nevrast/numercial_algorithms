426511d262f187a51666ee7c3da52a994e80133e
Patryk <pat.grzybala@gmail.com>
2020-03-09 16:56:37 +0100

    bakward_sub fix

--- classes/utils/utils.py
+++ classes/utils/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def backward_substitution(A: np.ndarray, b: np.ndarray) -> np.ndarray:
     n = b.shape[0]
-    x = np.zeros_like(b)
+    x = np.empty(b.shape)
 
     for i in range(n - 1, -1, -1):
         x[i] = (b[i] - sum(calculate_row(row=A[i], x=x)[i + 1:] if i != n - 1 else [])) / A[i, i]
@@ -13,6 +13,6 @@ def backward_substitution(A: np.ndarray, b: np.ndarray) -> np.ndarray:
 
 def calculate_row(row: np.ndarray, x: np.ndarray) -> np.ndarray:
     for index, x_value in enumerate(x):
-        if x_value != 0:
+        if x_value:
             row[index] *= x_value
     return row
